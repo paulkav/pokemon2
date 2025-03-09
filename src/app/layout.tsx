94ed8c1ed795f9807d02cac1a3e526392fe57365
Paul Kavaleuski <kavalbusiness@gmail.com>
2025-03-08 20:06:28 -0500

    Fix Metadata import in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
-import type { Metadata } from "next/types";
+import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
