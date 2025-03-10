a6c3c3ff1e848ea6546e7f98b0e5cde7a244d145
Paul Kavaleuski <kavalbusiness@gmail.com>
2025-03-09 22:34:26 -0400

    fixed links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,34 +1,31 @@
 'use client';
 
+import Link from 'next/link';
+
 const NavBar = () => {
   return (
-    <nav className="bg-gray-800 py-4">
-      <div className="flex justify-between items-center max-w-7xl mx-auto px-4">
-        <a 
-          href="http://localhost:3000" 
-          className="text-white no-underline text-xl font-bold"
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link 
+          href="/" 
+          className="text-xl font-bold hover:text-gray-300 transition-colors"
         >
-          Pokemon Lookup
-        </a>
-        <div>
-          <a 
-            href="http://localhost:3000" 
-            className="text-white no-underline mr-4 px-4 py-2"
-          >
-            Home
-          </a>
-          <a 
-            href="http://localhost:3000/compare" 
-            className="text-white no-underline mr-4 px-4 py-2"
+          Pokemon Explorer
+        </Link>
+        <div className="space-x-4">
+          <Link 
+            href="/compare" 
+            className="hover:text-gray-300 transition-colors"
           >
             Compare
-          </a>
-          <a 
-            href="http://localhost:3000/battle" 
-            className="text-white no-underline px-4 py-2"
+          </Link>
+          <Link 
+            href="/battle" 
+            className="hover:text-gray-300 transition-colors"
           >
             Battle
-          </a>
+          </Link>
+          {/* Add more navigation links as needed */}
         </div>
       </div>
     </nav>
